68dadb30bbcb7a5ae812e7e33f1d7892a4133041
girilakshmi <46717534+girilakshmi@users.noreply.github.com>
2019-01-17 21:47:07 +0100

    Update TicTocConsoleTest.java

--- tests/TicTocConsoleTest.java
+++ tests/TicTocConsoleTest.java
@@ -1,75 +1,73 @@
-
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class TicTocConsoleTest {
  static String[] table =new String[9];
 
- 	@Test
-	public void testBoardRowWin() {
+ @Test
+ public void testBoardRowWin() {
      table[0]="X";	
      table[1]="X";
      table[2]="X";     
      String expected ="X";
      String result=TicTocConsole.theWinner(table);
      assertTrue(expected.equals(result));     
-	}
+}
 
- 	@Test
-	public void testBoardRowDraw() {
+ @Test
+ public void testBoardRowDraw() {
      table[3]="X";	
      table[4]="X";
      table[5]="O";     
      String expected ="draw";
      String result=TicTocConsole.theWinner(table);
      assertTrue(expected.equals(result));     
-	}
+}
 
- 	@Test
-	public void testBoardColumnWin() {
+ @Test
+ public void testBoardColumnWin() {
      table[0]="O";	
      table[3]="O";
      table[6]="O";     
      String expected ="O";
      String result=TicTocConsole.theWinner(table);
      assertTrue(expected.equals(result));     
-	}
+}
 
- 	@Test
-	public void testBoardColumnDraw() {
+ @Test
+ public void testBoardColumnDraw() {
      table[0]="O";	
      table[3]="O";
      table[6]="X";     
      String expected ="draw";
      String result=TicTocConsole.theWinner(table);
      assertTrue(expected.equals(result));     
-	}
+}
 
- 	@Test
-	public void testBoardDiagonalWin() {
+ @Test
+ public void testBoardDiagonalWin() {
      table[0]="X";	
      table[4]="X";
      table[8]="X";     
      String expected ="X";
      String result=TicTocConsole.theWinner(table);
      assertTrue(expected.equals(result));     
-	}
+}
 
- 	@Test
-	public void testBoardDiagonalDraw() {
+ @Test
+ public void testBoardDiagonalDraw() {
      table[2]="O";	
      table[4]="X";
      table[6]="O";     
      String expected ="draw";
      String result=TicTocConsole.theWinner(table);
      assertTrue(expected.equals(result));     
-	}
+}
 
- 	@Test
- 	public void testNonEmptyBoard(){
+ @Test
+  public void testNonEmptyBoard(){
  		String[] expectedBoardVal=new String[9];
  		String[] actualBoardVal=new String[0];
  		assertArrayEquals(expectedBoardVal, actualBoardVal);
- 	}
+ }
 }
